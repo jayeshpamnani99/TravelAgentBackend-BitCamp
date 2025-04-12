b66a7e794c23857f9a40e138b6e8a2d59bd8f923
Pradyumna Upadhyay <pdupd28@umd.edu>
2025-04-12 15:40:08 -0500

    Modified CORS policy

--- app/fastapi_app.py
+++ app/fastapi_app.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from app.api.routes import router as api_router
 
-app = FastAPI(title="Agentverse 2.0")
+app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 app.include_router(api_router, prefix="/api/v1")
